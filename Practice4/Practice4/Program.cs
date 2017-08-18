681a57fa04f176cc45fcdccffc00150cb3f2b63c
Seongjin Cheon <yescsj@gmail.com>
2017-08-18 11:39:39 -0400

    Practice4 - Determine if a number is a power of 2

---
+++ Practice4/Practice4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice4
+{
+    class Program
+    {
+        //Determine if a number is a power of 2
+        static void Main(string[] args)
+        {
+            bool IsPowerOf2(int n)
+            {
+                return (n > 0) && ((n & (n - 1)) == 0);
+            }
+
+            for (int i = 0; i < 33; i++)
+            {
+                Console.WriteLine("{0} = {1}", i, IsPowerOf2(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
