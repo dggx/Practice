0fd329e1505a2f6aa85439cce464f03654982fee
Seongjin Cheon <yescsj@gmail.com>
2017-08-18 11:36:22 -0400

    Practice3 - Random number generator, Random from list

---
+++ Practice3/Practice3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random Number Generator
+
+            Random rnd = new Random();
+            int x = rnd.Next(1, 6);
+            int y = rnd.Next(1, 6);
+            int z = rnd.Next(1, 6);
+
+            int a = x + y + z;
+
+            Console.Write(a.ToString());
+
+            Console.Read();
+
+
+            //Random from list
+            //List<string> asdf = new List<string>();
+
+            //asdf.Add("one");
+            //asdf.Add("two");
+            //asdf.Add("three");
+            //asdf.Add("four");
+            //asdf.Add("five");
+
+            //Random r = new Random();
+
+            //int a = r.Next(asdf.Count);
+
+            //Console.Write((string)asdf[a]);
+
+            //Console.Read();
+        }
+    }
+}
